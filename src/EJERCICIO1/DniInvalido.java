f16073708813cd050c8901f759d83ac5a2dcb25e
ClaudioArielNunez <claudioariel_n@hotmail.com>
2024-09-03 23:31:38 -0300

    Primer commit

---
+++ src/EJERCICIO1/DniInvalido.java
@@ -0,0 +1,20 @@
+package EJERCICIO1;
+
+import java.io.IOException;
+
+public class DniInvalido extends IOException{
+
+	
+	//Constructor
+		public DniInvalido() {
+			
+		}
+
+		@Override
+		public String getMessage() {
+			// TODO Auto-generated method stub
+			return super.getMessage();
+		}
+		
+		
+}

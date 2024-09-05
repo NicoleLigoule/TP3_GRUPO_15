d692bdc5786a9c1f55928ec26361f567e7944e40
danimrios <danimrios09@gmail.com>
2024-09-05 16:26:47 -0300

    Create Persona.java

---
+++ src/EJERCICIO1/Persona.java
@@ -0,0 +1,30 @@
+package EJERCICIO1;
+
+public class Persona {
+	
+	private String Nombre; 
+	private String Apellido; 
+    private String  Dni ;
+    
+
+
+	///geters seters
+	public String getNombre() {
+		return Nombre;
+	}
+	public void setNombre(String nombre) {
+		Nombre = nombre;
+	}
+	public String getApellido() {
+		return Apellido;
+	}
+	public void setApellido(String apellido) {
+		Apellido = apellido;
+	}
+	public String getDni() {
+		return Dni;
+	}
+	public void setDni(String dni) {
+		Dni = dni;
+	}
+}

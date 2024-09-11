3774b05217221abfea9716dd3b12f040a2ca6195
Nicole <nicole.ligoule@alumnos.frgp.utn.edu.ar>
2024-09-10 22:23:10 -0300

    Revert "Agrego hashcode"
    This reverts commit 449137196b9654186c92ec1656e7a586c394f81f.

--- src/EJERCICIO1/Persona.java
+++ src/EJERCICIO1/Persona.java
@@ -48,39 +48,5 @@ public class Persona {
 		Apellido = apellido;
 		Dni = dni;
 	}
-    
-    @Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((Dni == null) ? 0 : Dni.hashCode());
-		return result;
-	}
-    
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Persona other = (Persona) obj;
-		if (Apellido == null) {
-			if (other.Apellido != null)
-				return false;
-		} else if (!Apellido.equals(other.Apellido))
-			return false;
-		if (Dni == null) {
-			if (other.Dni != null)
-				return false;
-		} else if (!Dni.equals(other.Dni))
-			return false;
-		if (Nombre == null) {
-			if (other.Nombre != null)
-				return false;
-		} else if (!Nombre.equals(other.Nombre))
-			return false;
-		return true;
-	}
+	
 }
